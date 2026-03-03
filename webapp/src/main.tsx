03059cff3f8dca67218d60e954272a3fcab4bdfe
Narcoz-eng <rengarro@gmail.com>
2026-03-04 01:09:42 +0200

    FIFL

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -2,6 +2,21 @@
 import { Buffer } from "buffer";
 window.Buffer = Buffer;
 
+if (typeof window !== "undefined") {
+  const host = window.location.hostname.toLowerCase();
+  const isVercelPreviewHost =
+    host.endsWith(".vercel.app") &&
+    host !== "phew.run" &&
+    host !== "www.phew.run" &&
+    !host.endsWith(".phew.run");
+
+  if (isVercelPreviewHost) {
+    const redirectUrl =
+      `https://www.phew.run${window.location.pathname}${window.location.search}${window.location.hash}`;
+    window.location.replace(redirectUrl);
+  }
+}
+
 import { createRoot } from "react-dom/client";
 import { ErrorBoundary } from "./components/ErrorBoundary";
 import App from "./App.js";
